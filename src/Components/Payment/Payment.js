aec8b7d862b63dc8ffc7cdda207234bdc8cf9f6b
Jevin Jivani <jivani.jevin25@gmail.com>
2023-08-20 14:59:47 +0530

    bug fized

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -27,6 +27,48 @@ function Payment({ closePopupFunction }) {
   const [itemPriceInEth, setItemPriceInEth] = useState(0);
 
   
+  const connectWallet = async () => {
+    try {
+      setProcessing(true);
+      if (window.ethereum) {
+        console.log("MetaMask is installed");
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.send("eth_requestAccounts");
+        // Get account address
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+
+        console.log("1. From MetaMask: ", accounts[0]);
+
+        if (accounts.length > 0) {
+          setBuyerAddress(accounts[0]);
+          setWalletConnected(true);
+        } else {
+          setWalletConnected(false);
+        }
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        const accounts = await window.web3.eth.getAccounts();
+        console.log("2. From MetaMask: ", accounts[0]);
+        if (accounts.length > 0) {
+          setBuyerAddress(accounts[0]);
+          setWalletConnected(true);
+        } else {
+          setWalletConnected(false);
+        }
+      } else {
+        console.log(
+          "Non-Ethereum browser detected. You should consider trying MetaMask!"
+        );
+        setWalletConnected(false);
+      }
+      setProcessing(false);
+    } catch (err) {
+      console.log(err);
+      setProcessing(false);
+    }
+  };
 
   useEffect(() => {
     if (paymentsData.show) {
